c4ab09b27c02e65c759b0b6e13fc194195114835
peleg <pelegs29@Gmail.com>
2021-06-07 22:26:26 +0300

    init

--- src/game/GameFlow.java
+++ src/game/GameFlow.java
@@ -1,31 +1,31 @@
 package game;
 
 import biuoop.KeyboardSensor;
-g
+
 import java.awt.List;
 
-//public class GameFlow {
-//    public GameFlow(AnimationRunner ar, KeyboardSensor ks, ...) { ... }
-//
-//    public void runLevels(List<LevelInformation> levels) {
-//        // ...
-//        for (LevelInformation levelInfo : levels) {
-//
-//            GameLevel level = new GameLevel(levelInfo,
-//                    this.keyboardSensor,
-//                    this.animationRunner,
-//               ...);
-//
-//            level.initialize();
-//
-//            while (level has more blocks and balls) {
-//                level.run();
-//            }
-//
-//            if (no more balls) {
-//                break;
-//            }
-//
-//        }
-//    }
-//}
+public class GameFlow {
+    public GameFlow(AnimationRunner ar, KeyboardSensor ks, ...) { ... }
+
+    public void runLevels(List<LevelInformation> levels) {
+        // ...
+        for (LevelInformation levelInfo : levels) {
+
+            GameLevel level = new GameLevel(levelInfo,
+                    this.keyboardSensor,
+                    this.animationRunner,
+               ...);
+
+            level.initialize();
+
+            while (level has more blocks and balls) {
+                level.run();
+            }
+
+            if (no more balls) {
+                break;
+            }
+
+        }
+    }
+}
